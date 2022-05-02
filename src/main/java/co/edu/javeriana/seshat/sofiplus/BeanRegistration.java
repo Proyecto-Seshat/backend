f9970c1e34802024b508fd647083622fde97a8cb
Jairo Vanegas <jairo.vanegas@javeriana.edu.co>
2022-05-01 22:33:25 -0500

    Bean registration corregido

--- src/main/java/co/edu/javeriana/seshat/sofiplus/BeanRegistration.java
+++ src/main/java/co/edu/javeriana/seshat/sofiplus/BeanRegistration.java
@@ -26,29 +26,29 @@ public class BeanRegistration implements BeanDefinitionRegistryPostProcessor {
     @Override
     public void postProcessBeanDefinitionRegistry(
             BeanDefinitionRegistry bdr) throws BeansException {
-        bdr.registerBeanDefinition(GetFactura.class.getCanonicalName(), BeanDefinitionBuilder.genericBeanDefinition(GetFactura.class).setScope("prototype").getBeanDefinition());
-        bdr.registerBeanDefinition(RegisterCliente.class.getCanonicalName(), BeanDefinitionBuilder.genericBeanDefinition(RegisterCliente.class).setScope("prototype").getBeanDefinition());
-        bdr.registerBeanDefinition(GetCliente.class.getCanonicalName(), BeanDefinitionBuilder.genericBeanDefinition(GetCliente.class).setScope("prototype").getBeanDefinition());
-        bdr.registerBeanDefinition(TrollAgent.class.getCanonicalName(), BeanDefinitionBuilder.genericBeanDefinition(TrollAgent.class).setScope("prototype").getBeanDefinition());
-//        try {
-//            File f = new File("./src/main/java/co/edu/javeriana/seshat/sofiplus/Modules/src");
-//            for (File module : Objects.requireNonNull(f.listFiles())) {
-//                File fMethod = new File(module.getAbsolutePath() + "/Methods/");
-//                for (File method :
-//                        Objects.requireNonNull(fMethod.listFiles())) {
-//                    URL[] cp = {method.toURI().toURL()};
-//                    System.out.println(Arrays.toString(cp));
-//                    try (URLClassLoader urlcl = new URLClassLoader(cp)) {
-//                        Class<? extends ModuleRunnable> methodClass = (Class<? extends ModuleRunnable>) urlcl.loadClass("co.edu.javeriana.seshat.sofiplus.Modules.src." + module.getName() + ".Methods." + method.getName());
-//                        bdr.registerBeanDefinition(methodClass.getCanonicalName(), BeanDefinitionBuilder.genericBeanDefinition(methodClass).setScope("prototype").getBeanDefinition());
-//                    } catch (IOException e) {
-//                        throw new RuntimeException(e);
-//                    }
-//                }
-//            }
-//        } catch (MalformedURLException | ClassNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
+//        bdr.registerBeanDefinition(GetFactura.class.getCanonicalName(), BeanDefinitionBuilder.genericBeanDefinition(GetFactura.class).setScope("prototype").getBeanDefinition());
+//        bdr.registerBeanDefinition(RegisterCliente.class.getCanonicalName(), BeanDefinitionBuilder.genericBeanDefinition(RegisterCliente.class).setScope("prototype").getBeanDefinition());
+//        bdr.registerBeanDefinition(GetCliente.class.getCanonicalName(), BeanDefinitionBuilder.genericBeanDefinition(GetCliente.class).setScope("prototype").getBeanDefinition());
+//        bdr.registerBeanDefinition(TrollAgent.class.getCanonicalName(), BeanDefinitionBuilder.genericBeanDefinition(TrollAgent.class).setScope("prototype").getBeanDefinition());
+        try {
+            File f = new File("./src/main/java/co/edu/javeriana/seshat/sofiplus/Modules/src");
+            for (File module : Objects.requireNonNull(f.listFiles())) {
+                File fMethod = new File(module.getAbsolutePath() + "/Methods/");
+                for (File method :
+                        Objects.requireNonNull(fMethod.listFiles())) {
+                    URL[] cp = {method.toURI().toURL()};
+                    try (URLClassLoader urlcl = new URLClassLoader(cp)) {
+                        String methodName = method.getName().substring(0, method.getName().lastIndexOf('.'));
+                        Class<? extends ModuleRunnable> methodClass = (Class<? extends ModuleRunnable>) urlcl.loadClass("co.edu.javeriana.seshat.sofiplus.Modules.src." + module.getName() + ".Methods." + methodName);
+                        bdr.registerBeanDefinition(methodClass.getCanonicalName(), BeanDefinitionBuilder.genericBeanDefinition(methodClass).setScope("prototype").getBeanDefinition());
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        } catch (MalformedURLException | ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
