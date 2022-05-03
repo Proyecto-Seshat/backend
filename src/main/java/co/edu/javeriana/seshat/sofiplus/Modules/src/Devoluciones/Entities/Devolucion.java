6ab751fd9900bfc43282ce0d097763c230a72cb5
juansebas207 <62665371+juansebas207@users.noreply.github.com>
2022-05-02 20:34:43 -0500

    commit desde github desktop

---
+++ src/main/java/co/edu/javeriana/seshat/sofiplus/Modules/src/Devoluciones/Entities/Devolucion.java
@@ -0,0 +1,7 @@
+package co.edu.javeriana.seshat.sofiplus.Modules.src.Devoluciones.Entities;
+
+import java.util.List;
+
+public class Devolucion {
+
+}
---
+++ src/main/java/co/edu/javeriana/seshat/sofiplus/Modules/src/Devoluciones/ModuleDevolucion.java
@@ -0,0 +1,4 @@
+package co.edu.javeriana.seshat.sofiplus.Modules.src.Devoluciones;
+
+public class ModuleDevolucion {
+}
